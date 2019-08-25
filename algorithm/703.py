c8a9a5f91cb0d044c97feea41c021332112a0334
wangchuangbin <406275301@qq.com>
2019-08-25 23:39:26 +0800

    review

---
+++ algorithm/703.py
@@ -0,0 +1,18 @@
+import heapq
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.pool = heapq.nlargest(k, nums)[::-1]
+        self.k = k
+        
+
+    def add(self, val):
+        if len(self.pool) < self.k:
+            heapq.heappush(self.pool, val)
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
